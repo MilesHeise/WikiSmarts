7e677c6c3e14342fb3d190c487cb1b5d6631b5ff
Miles Heise <milesheise@gmail.com>
2017-11-30 13:44:03 -0500

    markdown text enabled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def markdown(text)
+    options = {
+      escape_html:     true,
+      hard_wrap:       true
+    }
+
+    extensions = {
+      no_intra_emphasis: true,
+      strikethrough: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
